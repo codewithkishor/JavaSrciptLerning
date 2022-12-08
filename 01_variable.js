901f983d2af02eea36179bb51d5c3de4f5f7a3bb
codewithkishor <bkishor306@gmail.com>
2022-12-08 20:38:42 +0530

    New assignment added

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+var myName; // Variable Declaration
+myName = "Kishor Bargaje"; //Variable initializastion
+console.log(myName)
+var myFrndName = "Sachin";
+console.log(myFrndName)
+
+myFrndName = "MS Dhoni";
+console.log(myFrndName)
---
+++ 01_variable_assignment.js
@@ -0,0 +1,9 @@
+var myFullName; // Variable Declaration
+myFullName = "Kishor Bargaje"; //Variable initializastion
+console.log(myFullName)
+
+myFullName = "Kishor Navnath Bargaje";
+console.log(myFullName)
+
+var myLove= "I Love only Js";
+console.log(myLove)
