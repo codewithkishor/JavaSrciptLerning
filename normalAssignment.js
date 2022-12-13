a2536e826354172a59a2cfa0a30a9c849c080430
codewithkishor <bkishor306@gmail.com>
2022-12-13 09:36:09 +0530

    String in Js learning
    String in Js learning

--- normalAssignment.js
+++ normalAssignment.js
@@ -1,9 +1,18 @@
-function bankDetails(bankName, accountNum, location, pinCode){
-    console.log("It's CodeMind bank Details make your payment on this -", bankName, accountNum, location, pinCode);
-    console.log("It's my bank Details make payment on this-", bankName, accountNum, location, pinCode);
-    console.log("Its my bank Details make payment on this-", bankName, accountNum, location, pinCode);
-
+console.log("Below are the bank deatails for payment");
+function bankDetails(bankName, accountNum, location, pinCode) {
+  console.log(
+    "Bank Name-",
+    bankName,
+    "Account Number-",
+    accountNum,
+    "Location-",
+    location,
+    "Pin Code-",
+    pinCode
+  );
 }
-bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
-bankDetails ("Axis Bank", 7856782345, 'Mumbai', 441202);
-bankDetails("HDFC Bank", 8956782345, 'Pune', 631202, "Open");
+bankDetails("CITI Bank", 3456782345, "Pune", 431202);
+
+bankDetails("Axis Bank", 7856782345, "Mumbai", 441202);
+
+bankDetails("HDFC Bank", 8956782345, "Pune", 631202);
